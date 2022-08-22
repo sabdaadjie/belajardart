738c437c15701a48c3feaa2c5886a0702c01a763
sabdaadjie <55336512+sabdaadjie@users.noreply.github.com>
2022-08-22 11:18:33 +0700

    first commit

---
+++ lib/hello.dart
@@ -0,0 +1,11 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library hello;
+
+export 'src/hello_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
+void main() {
+  print("hello world");
+}
